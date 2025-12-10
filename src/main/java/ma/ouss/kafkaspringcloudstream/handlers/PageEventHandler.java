711657f973c0867e9dbb18f88060368cc95c2d00
oussama bissi <oussama_bissi@sigr.gr>
2025-12-10 14:13:59 +0100

    feat: imlplement code for The PageEventSupplier

--- src/main/java/ma/ouss/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/ma/ouss/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -1,7 +1,32 @@
 package ma.ouss.kafkaspringcloudstream.handlers;
 
+import ma.ouss.kafkaspringcloudstream.events.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  * @author $ {USER}
  **/
+@Component
 public class PageEventHandler {
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input)->{
+            System.out.println("*******************");
+            System.out.println(input.toString());
+            System.out.println("*******************");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return ()->{
+            return new PageEvent(Math.random()>0.5?"P1":"P2",Math.random()>0.5 ? "U1":"U2",new Date(),new Random().nextInt(1000));
+        };
+    }
 }
